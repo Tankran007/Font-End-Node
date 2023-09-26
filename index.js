59856dfb9b65bbc650334d5233bb85a4a47fda0d
Tankran007 <s6406021622150@email.kmutnb.ac.th>
2023-09-26 17:22:56 +0700

    dsadsa

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 var bodyParser = require("body-parser");
+const path = require("path");
+
 
 
 // Base URL for the API
@@ -10,11 +12,13 @@ const base_url = "http://localhost:3000";
 //const base_url = "http://node41091-noderest.proen.app.ruk-com.cloud";
 
 // Set the template engine
+app.set("views", path.join(__dirname,"/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Serve static files
+
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", async (req, res) => {
